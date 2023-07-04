cba4999987878a32e6f51427e5a4d477accab79e
Alex Sandro Dabukke <129069950+mrsidabukke@users.noreply.github.com>
2023-07-04 11:05:10 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php 2</title>
+</head>
+<body style="background-color: grey;">
+<a href="index2.php">website php ke 2</a><br>
+    <a href="index1.php">website php ke 1</a><br>
+    <h1><strong>belajar php2</strong></h1>
+    
+
+    <?php  echo "selamat datang di website php ke 2";
+    echo"<br>";
+    echo"<br>";
+    echo"<br>";
+    
+    function br (){
+        echo"<br>";
+        echo"<br>";
+        echo"<br>";
+    }
+
+    
+
+     
+     
+
+     
+
+        
+
+
+    
+    
+    
+    ?>
+    
+    
+</body>
+</html>
